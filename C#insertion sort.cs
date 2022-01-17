535507645703c00dbcccb94e65bcc9ce2a3cf5df
birtaa <97830462+birtaa@users.noreply.github.com>
2022-01-17 20:12:47 +0300

    Add files via upload

---
+++ C#insertion sort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace csarp_kokletturmece
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+            int[] array = new int[5] { 13, 6, 50, 4, 31 };
+
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < array[i - 1])
+                {
+                    int a = i;
+
+                    while (array[a] < array[a - 1])
+                    {
+                        temp = array[a];
+                        array[a] = array[a - 1];
+                        array[a - 1] = temp;
+                        a--;
+                        if (a == 0)
+                        {
+                            break;
+                        }
+
+                    }
+                }
+
+            }
+
+            Console.WriteLine(array[0]);
+            Console.WriteLine(array[1]);
+            Console.WriteLine(array[2]);
+            Console.WriteLine(array[3]);
+            Console.WriteLine(array[4]);
+
+        }
+    }
+}
